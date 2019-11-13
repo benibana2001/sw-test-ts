50bef8cb5c8c293d9f753886c2dfacf2df1dab15
benibana2001 <benibana2001@gmail.com>
2019-11-14 05:31:38 +0900

    hello service worker by work-box

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
-console.log('Hello world!')
-// register service worker
-// if ('serviceWorker' in navigator) {
-    // navigator.serviceWorker.register('sw.ts', {scope: '/'})
-// }
+console.log('Hellooo world!')
+console.log(navigator)
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js').then(registration => {
+            console.log('SW registered: ', registration);
+        }).catch(registrationError => {
+            console.log('SW registration failed: ', registrationError);
+        });
+    });
+} else {
+    console.log("no serviceWorker")
+}
